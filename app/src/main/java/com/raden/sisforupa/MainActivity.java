678ff941ccb29d2d3451fb855c3dadc4ec8a5db4
radenfatahillah <radenfatahillah@student.untan.ac.id>
2019-10-03 20:03:43 +0700

    Tambahkan Menu Telpon dan Maps

--- app/src/main/java/com/raden/sisforupa/MainActivity.java
+++ app/src/main/java/com/raden/sisforupa/MainActivity.java
@@ -15,10 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnTelpon = findViewById(R.id.btnTelpon);
+        Button btnMaps = findViewById(R.id.btnMaps);
 
         btnTelpon.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 telponNomor(v);
+            }
+        });
+
+        btnMaps.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                bukaMaps(v);
             }
         });
     }
@@ -44,13 +51,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void telponNomor(View view){
-        Uri uri = Uri.parse("tel:085245894354");
+        Uri uri = Uri.parse("tel:081255180007");
         Intent it = new Intent(Intent.ACTION_DIAL, uri);
         startActivity(it);
     }
 
-    public void buttonIkon2(View view){
-        Intent intent = new Intent(MainActivity.this, Ikon2Activity.class);
+    public void bukaMaps(View view){
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("geo:0,0?q=-0.056777,109.344883"));
         startActivity(intent);
     }
 
